c0ede2cfcca01eae42ad5402af9f400e389441b9
sasha <o.l@smoothr.de>
2023-11-28 23:22:43 +0200

    handle end of the video

--- streaming_utils.py
+++ streaming_utils.py
@@ -14,9 +14,12 @@ class TestFrameCapture:
         self.fps_r = self.cap_r.get(cv2.CAP_PROP_FPS)
 
     def get_frame(self):
-        time_elapsed = time.monotonic() - self.start_time
-        frame_num_l = int(time_elapsed * self.fps_l)
-        frame_num_r = int(time_elapsed * self.fps_r)
-        self.cap_l.set(cv2.CAP_PROP_POS_FRAMES, frame_num_l)
-        self.cap_r.set(cv2.CAP_PROP_POS_FRAMES, frame_num_r)
-        return get_stitched_images(self.cap_r, self.cap_l, time_elapsed), time_elapsed
+        try:
+            time_elapsed = time.monotonic() - self.start_time
+            frame_num_l = int(time_elapsed * self.fps_l)
+            frame_num_r = int(time_elapsed * self.fps_r)
+            self.cap_l.set(cv2.CAP_PROP_POS_FRAMES, frame_num_l)
+            self.cap_r.set(cv2.CAP_PROP_POS_FRAMES, frame_num_r)
+            return get_stitched_images(self.cap_r, self.cap_l, time_elapsed), time_elapsed
+        except ValueError:
+            exit(0)
